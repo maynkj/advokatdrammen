af3abb6986993bb07df27c9db0229b914b48ef41
maynkj <mayn.kjaer@gmail.com>
2019-01-22 21:24:48 +0100

    Updates

---
+++ precache-manifest.c6efac741ed28b369e3d9e79dca0e84d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "631af4eb20c1f6419c2c",
+    "url": "/myapp/static/css/main.83afbd12.chunk.css"
+  },
+  {
+    "revision": "631af4eb20c1f6419c2c",
+    "url": "/myapp/static/js/main.631af4eb.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/myapp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "f1ac0442a5b395a34ff2dac866606a54",
+    "url": "/myapp/index.html"
+  }
+];
