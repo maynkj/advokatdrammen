dd46ebc3a70b7b15d85f0b92f1d3d2564a3da799
maynkj <mayn.kjaer@gmail.com>
2019-01-23 09:29:19 +0100

    Fix errors in content.

--- src/pages/mainPages/About.jsx
+++ src/pages/mainPages/About.jsx
@@ -4,20 +4,27 @@ export default () => (
   <React.Fragment>
     <h1>Om oss</h1>
     <p>
-      <em>Thorgeier Kjær</em> har vært advokat siden 1990. Advokat Kjær arbeider vesentlig med trygderett, erstatningsrett med hovedvekt
+      <strong>Thorgeier Kjær</strong> har vært advokat siden 1990. Advokat Kjær arbeider vesentlig med trygderett, erstatningsrett med hovedvekt
       på personskade, samt arv og familierett. Blant annet tar han klagesaker knyttet til NAV og barnevern. Han tar også oppdrag
-      som bistandsadvokat i straffesaker. Ta kontakt for en uforpliktende samtale på tlf:
-      <a href="tel:95248547">952 48 547</a> eller e-post:
-      <a href="https://web.archive.org/web/20180827230817/mailto:post@advokatdrammen.no">post@advokatdrammen.no</a>
+      som bistandsadvokat i straffesaker.
     </p>
     <p>
-      <em>Jon Gisle</em> har vært advokat siden 2008. Advokat Gisle arbeider vesentlig med trygderettslige og arbeidsrettslige saker.
+      Ta kontakt for en uforpliktende samtale på
+      tlf: <a href="tel:95248547">952 48 547</a>
+      eller e-post: <a href="https://web.archive.org/web/20180827230817/mailto:post@advokatdrammen.no">post@advokatdrammen.no</a>
+    </p>
+    <p>
+      <strong>Jon Gisle</strong> har vært advokat siden 2008. Advokat Gisle arbeider vesentlig med trygderettslige og arbeidsrettslige saker.
       Blant annet tar han saker om uføretrygd, arbeidsavklaringspenger og sykepenger, og om ansettelse, oppsigelse, avskjed og
-      arbeidstid, samt tariffsaker.</p>
-    <p>Tidligere jobbet Gisle i Arbeidsretten som dommer og rettens nestleder. Han er også hovedforfatter på Kunnskapsforlagets
-      Jusleksikon, som kom i 5. utgave 2017. Ta kontakt på mobil
-      <a href="tel:48122740">481 22 740</a> eller e-post:
-      <a href="https://web.archive.org/web/20180827230817/mailto:jon@arbeidsrettsadvokatene.no">jon@arbeidsrettsadvokatene.no</a>. </p>
+      arbeidstid, samt tariffsaker.
+    </p>
+    <p>
+      Tidligere jobbet Gisle i Arbeidsretten som dommer og rettens nestleder. Han er også hovedforfatter på Kunnskapsforlagets
+      Jusleksikon, som kom i 5. utgave 2017.
+    <p>
+      Ta kontakt på mobil <a href="tel:48122740">481 22 740</a>
+      eller e-post:<a href="https://web.archive.org/web/20180827230817/mailto:jon@arbeidsrettsadvokatene.no">jon@arbeidsrettsadvokatene.no</a>. </p>
+    </p>
     <p>Advokatkontorets fagfelt strekker seg innen de fleste juridiske områder, samt eiendomsmegling. Advokatkontoret bistår så
       vel privatpersoner som større og mindre foretak. Vi satser på rask service, og en personlig og tett oppfølgning av våre
       klienter. </p>
--- src/pages/mainPages/FreeCounsel.jsx
+++ src/pages/mainPages/FreeCounsel.jsx
@@ -10,7 +10,7 @@ export default () => (
       I noen typer saker kan alle få fri rettshjelp. I andre saker er det inntekt og formue som avgjør om du kommer inn under ordningen. Nærmere informasjon om fri rettshjelp kan fås hos Fylkesmannen eller ved vårt kontor.
     </p>
     <p>
-      For fri rettshjelp gjelder inntektsgrenser. For å få fri rettshjelp kan klienten ikke ha bruttoinntekt over kr. 246.000 for enslige og kr. 369.000 for ektefeller eller andre som lever sammen med felles økonomi. Formuesgrensen er kr 100.000. De økonomiske vilkårene for innvilgelse av fri rettshjelp følger av forskrift til lov om fri rettshjelp som ligger på lovdata. På <a href="http://www.lovdata.no/Drift-av-advokatvirksomhet/ Klientforholdet/Rettshjelpsforsikring/">lovdata.no</a> kan man også gratis søke i alle Norges lover og forskrifter.
+      For fri rettshjelp gjelder inntektsgrenser. For å få fri rettshjelp kan klienten ikke ha bruttoinntekt over kr. 246.000 for enslige og kr. 369.000 for ektefeller eller andre som lever sammen med felles økonomi. Formuesgrensen er kr 100.000. De økonomiske vilkårene for innvilgelse av fri rettshjelp følger av <a href="https://lovdata.no/dokument/SF/forskrift/2005-12-12-1443">forskrift til lov om fri rettshjelp</a> som ligger på lovdata. På <a href="http://www.lovdata.no">lovdata.no</a> kan man også gratis søke i alle Norges lover og forskrifter.
     </p>
     <p>
       Vi vil være behjelpelige med å finne ut om man kan få dekket utgiftene til rettshjelp i de tilfeller man er usikker på det.
