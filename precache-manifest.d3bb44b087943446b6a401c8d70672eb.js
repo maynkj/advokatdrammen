92e549cd456d79c08b0b1d8b76e073657b986ec1
maynkj <maynkj@protonmail.com>
2020-05-20 09:06:14 +0200

    Updates

---
+++ precache-manifest.d3bb44b087943446b6a401c8d70672eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60caad70e29dcf1993fb",
+    "url": "/static/css/main.05f7a188.chunk.css"
+  },
+  {
+    "revision": "60caad70e29dcf1993fb",
+    "url": "/static/js/main.60caad70.chunk.js"
+  },
+  {
+    "revision": "45c83b28f88ef9bb2513",
+    "url": "/static/js/1.45c83b28.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "702ab702ccff86b0e1a5e062b39d1647",
+    "url": "/static/media/logo.702ab702.gif"
+  },
+  {
+    "revision": "dd5ddaec263fa0209c5edaa17649917f",
+    "url": "/index.html"
+  }
+];
